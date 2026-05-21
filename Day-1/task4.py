d7dd1ad3fccceab57e2320b1ad68c4bc3cc62995
OM KUSHWAHA <omkushwaha2005@gmail.com>
2026-05-22 04:00:15 +0530

    task4

---
+++ Day-1/task4.py
@@ -0,0 +1,12 @@
+#first variable
+name = "om"
+print(name)
+#second variable
+name = "noor"
+print(name)
+
+#print(len(input("what is your name?")))
+username = input("what is your name?")
+length = len(username)
+print(length)
+
