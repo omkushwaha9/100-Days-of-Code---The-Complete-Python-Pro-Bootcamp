7452be4009b930efd9c630feae4d926e92999709
OM KUSHWAHA <omkushwaha2005@gmail.com>
2026-05-23 05:05:06 +0530

    Task3

---
+++ Day-2/Task3.py
@@ -0,0 +1,26 @@
+print("My age: " + str(21))
+#Addition
+print(123 + 456)
+#Substraction
+print(4 - 2)
+#Multiplication
+print(3 * 3)
+#Division
+print(6 / 3)
+#For no integer Divide
+print(5 // 3)
+#For Root
+print(5 ** 3)
+
+
+#PEMDAS = Parentheses, Exponents, Multiplication/Division, Addition/Subtraction
+# PEMDAS-LR Order
+# ()
+# **
+# * OR /
+# + OR -
+
+print(3 * 3 + 3 / 3 - 3)
+#Output is 7.0 as Left to Right Order
+#for output 3 change the Code
+print(3 * 3 + 3 / 3 - 7)
