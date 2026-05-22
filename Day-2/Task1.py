eeb0af0e0600fd2857a8bdbe8f8c04782f094818
OM KUSHWAHA <omkushwaha2005@gmail.com>
2026-05-23 04:29:04 +0530

    task1

---
+++ Day-2/Task1.py
@@ -0,0 +1,13 @@
+#Everything in between "" is a string
+#If we want to know the datatype we can use - Type (ex:)
+print(type("Hello"))
+#Output = <class 'str'>
+print(type(123))
+#Output = <class 'int'>
+print(type(09.2603))
+#Output = <class 'float'>
+print(type(True))
+#Output = <class 'bool'>
+print(type(123_456_789))
+#Output = <class 'int'>
+#Collection of major datatype's
