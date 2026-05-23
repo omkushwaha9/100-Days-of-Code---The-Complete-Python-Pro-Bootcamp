8148d7ed758c791c234520a92702ec53bf722d1b
OM KUSHWAHA <omkushwaha2005@gmail.com>
2026-05-23 05:48:17 +0530

    Task4

---
+++ Day-2/Task4.py
@@ -0,0 +1,14 @@
+bmi = 84 / 1.65 ** 2
+
+print(bmi)
+print(int(bmi))
+print(round(bmi))
+print(round(bmi,2))
+print("\n")
+
+#f-Strings
+score = 91
+Height = 5.9
+is_winning = True
+
+print(f"Your Score is {score} & Your Height is {Height} &\nYour Winning is {is_winning}")
