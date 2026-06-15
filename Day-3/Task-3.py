90e6ec713f5cd6171909c089e9f364c3ba87d55b
OM KUSHWAHA <omkushwaha2005@gmail.com>
2026-06-15 12:24:09 +0530

    Fix age-based pricing for rollercoaster ride

--- Day-3/Task-3.py
+++ Day-3/Task-3.py
@@ -4,9 +4,11 @@ height = int(input("What is your height in cm? "))
 if height >= 120:
     print("You can ride the rollercoaster")
     age = int(input("What is your age?"))
-    if  age <=18:
+    if  age <= 12:
+        print("please pay $5.")
+    elif age <= 18:
         print("please pay $7.")
     else:
-        print("please pay $18.")
+        print("please pay $12.")
 else:
     print("Sorry you have to grow taller before you can ride.")
